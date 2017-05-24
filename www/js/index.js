3f1b7491bb2813ac274e0c333941884cc931ec78
SeoChangHo <sch1774@nate.com>
2017-05-24 16:12:14 +0900

    1612

--- www/js/index.js
+++ www/js/index.js
@@ -1,19 +1,15 @@
-init = function(){
+
+var init = function(){
 	
-	 $("#owl-demo").owlCarousel({
+	$("#owl-demo").owlCarousel({
+			 
+		      autoPlay: 100000, //Set AutoPlay to 3 seconds
 		 
-	      autoPlay: 100000, //Set AutoPlay to 3 seconds
-	 
-	      items : 1,
-	      itemsDesktop : [1199,3],
-	      itemsDesktopSmall : [979,3]
-	 
-	  });
-}
+		      items : 1,
+		      itemsDesktop : [1199,3],
+		      itemsDesktopSmall : [979,3]
+	 });
+ }
 
 $(document).on("pagebeforecreate", init);
-
-
-
-
 
