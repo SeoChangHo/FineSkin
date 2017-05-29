bdd56534ed06b2fb6b03fb65999a4f92d9a4deb3
SeoChangHo <sch1774@nate.com>
2017-05-29 19:07:39 +0900

    1907

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,2 @@
 
-var init = function(){
-	
-	$("#owl-demo").owlCarousel({
-			 
-		      autoPlay: 100000, //Set AutoPlay to 3 seconds
-		 
-		      items : 1,
-		      itemsDesktop : [1199,3],
-		      itemsDesktopSmall : [979,3]
-	 });
- }
-
-$(document).on("pagebeforecreate", init);
 
