c04bd35721cdbda925bbcd31b042e637918b7d81
SeoChangHo <sch1774@nate.com>
2017-06-15 17:43:37 +0900

    1743

--- www/js/index.js
+++ www/js/index.js
@@ -29,10 +29,7 @@ function successCallback(position) {
         var sijuso = "";
         var gujuso = "";
         
-        
-        $("<div>").css({
-        	margin:"5px"
-        }).html(lat + ", " + lng).appendTo($("#location"));
+        console.log(lat + ", " + lng);
         
         $.ajax({
 	        url:"http://api.openweathermap.org/data/2.5/weather?lat=" +lat + "&lon=" + lng +"&APPID=92e7b2df78fb8de10187e0abd8c5a46b",
@@ -40,18 +37,28 @@ function successCallback(position) {
 	        dataType:"json",
 	        success:function(data){
 	           $(data).each(function(){
-	              var weather = this.weather[0].main;
-	              var sky = this.weather[0].description;
-	              var temp = this.main.temp - 273.15;
-	              var pressure = this.main.pressure;
-	              var humidity = this.main.humidity;
-	              var clouds = this.clouds.all;
-	              	
-	              if(weather == "Haze"){
-	            	  $("<div>").css({
-	                      margin: "5px"
-	                   }).html("날씨 : " + weather + " 온도 : " + temp.toFixed(2) + " 습도 : " + humidity ).appendTo($("#weather"));
-	              }	             
+	              var weather = this.weather[0].main;    //날씨
+	              var sky = this.weather[0].description;   //하늘상태
+	              var temp = this.main.temp - 273.15;   //기온
+	              var pressure = this.main.pressure;       //압력
+	              var humidity = this.main.humidity;      //습도
+	              var clouds = this.clouds.all;				//구름
+	              
+	            	  
+	            	  if(weather=="Clear"){
+	            		  $(".wlist_txt1").html("날씨 : 맑음");  
+	            	  }else if(weather=="Clouds"){
+	            		  $(".wlist_txt1").html("날씨 : 구름");
+	            	  }else if(weather=="Haze"){
+	            		  $(".wlist_txt1").html("날씨 : 흐림");
+	            	  }else if(weather=="Rain"){
+	            		  $(".wlist_txt1").html("날씨 : 비");
+	            	  }else{
+	            		  $(".wlist_txt1").html("날씨 : ?");
+	            	  }
+	            	  
+	            	  $(".wlist_txt2").html("온도 : " + temp.toFixed(2));
+	            	  $(".wlist_txt3").html("습도 : " + humidity);
 	           });
 	        },
 	        error: function(xhr, textStatus, errMsg){
@@ -66,13 +73,12 @@ function successCallback(position) {
 	        dataType:"json",
 	        success:function(data){
 	           $(data).each(function(){
-	        	   sijuso = this.name1;
-	              gujuso = this.name2;
-	              var name3 = this.name3;
-	              	
-            	  $("<div>").css({
-                      margin: "5px"
-                   }).html(sijuso + " " + gujuso + " " + name3 ).appendTo($("#juso"));
+	        	   sijuso = this.name1;   //시주소
+	              gujuso = this.name2;  //구주소
+	              var name3 = this.name3; //동주소
+	              
+            	  console.log(sijuso + " " + gujuso + " " + name3);
+            	  $(".location").html(gujuso + " " + name3);
 	           });
 	        },
 	        error: function(xhr, textStatus, errMsg){
@@ -88,11 +94,18 @@ function successCallback(position) {
 	        dataType:"json",
 	        success:function(data){
 	           $(data).each(function(){
-	        	   var mise = this.list[0].pm25Grade;
-
-            	  $("<div>").css({
-                      margin: "5px"
-                   }).html("미세먼지 지수 : " + mise).appendTo($("#mise"));
+	        	   var mise = this.list[0].pm25Grade; 		  //미세먼지등급
+	        	   var misenong = this.list[0].pm10Value; //미세먼지농도
+	        	   
+	        	  if(mise==1){
+	        		  $(".wlist_txt5").html("미세먼지: 좋음");  
+	        	  }else if(mise==2){
+	        		  $(".wlist_txt5").html("미세먼지: 보통");
+	        	  }else if(mise==3){
+	        		  $(".wlist_txt5").html("미세먼지: 나쁨");
+	        	  }else if(mise==4){
+	        		  $(".wlist_txt5").html("미세먼지:매우나쁨");
+	        	  }
 	           });
 	        },
 	        error: function(xhr, textStatus, errMsg){
@@ -106,11 +119,20 @@ function successCallback(position) {
 	        dataType:"json",
 	        success:function(data){
 	           $(data).each(function(){
-	        	   var uv = this.Response.body.indexModel.today;
+	        	   var uv = this.Response.body.indexModel.today; //자외선지수
 
-            	  $("<div>").css({
-                      margin: "5px"
-                   }).html("자외선 지수 : " + uv).appendTo($("#uv"));
+	        	if(uv<=2){
+	        		$(".wlist_txt4").html("자외선: 낮음");
+	        	}else if(uv<=5){
+	        		$(".wlist_txt4").html("자외선: 보통");
+	        	}else if(uv<=7){
+	        		$(".wlist_txt4").html("자외선: 높음");
+	        	}else if(uv<=10){
+	        		$(".wlist_txt4").html("자외선:매우높음");
+	        	}else{
+	        		$(".wlist_txt4").html("자외선: 위험");
+	        	}
+
 	           });
 	        },
 	        error: function(xhr, textStatus, errMsg){
