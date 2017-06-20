652725d484a5ce8dddc9f40b32419787e889593a
SeoChangHo <sch1774@nate.com>
2017-06-20 09:54:52 +0900

    0954

--- www/js/index.js
+++ www/js/index.js
@@ -39,9 +39,10 @@ function successCallback(position) {
         console.log(lat + ", " + lng);
         
         $.ajax({
-	        url:"http://api.openweathermap.org/data/2.5/weather?lat=" +lat + "&lon=" + lng +"&APPID=92e7b2df78fb8de10187e0abd8c5a46b",
+	        url:"//api.openweathermap.org/data/2.5/weather?lat=" +lat + "&lon=" + lng +"&APPID=92e7b2df78fb8de10187e0abd8c5a46b",
 	        type: "get",
-	        dataType:"json",
+	        crossDomain: true,
+	        dataType:"jsonp",
 	        success:function(data){
 	           $(data).each(function(){
 	              var weather = this.weather[0].main;    //날씨
@@ -76,10 +77,11 @@ function successCallback(position) {
 	     })
 	     
 	     $.ajax({
-	        url:"https://apis.daum.net/local/geo/coord2addr?apikey=b9ab1680e352b1d4c5fe2b578928060e&longitude=" + lng + "&latitude=" + lat + "&inputCoordSystem=WGS84&output=json",
+	        url:"//apis.daum.net/local/geo/coord2addr?apikey=b9ab1680e352b1d4c5fe2b578928060e&longitude=" + lng + "&latitude=" + lat + "&inputCoordSystem=WGS84&output=json",
 	        async:false,
 	        type: "get",
-	        dataType:"json",
+	        dataType:"jsonp",
+	        crossDomain: true,
 	        success:function(data){
 	           $(data).each(function(){
 	        	   sijuso = this.name1;   //시주소
@@ -98,9 +100,10 @@ function successCallback(position) {
 	     var gugugu = encodeURIComponent(gujuso);
 	     
 	     $.ajax({
-	    	url: "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=" + gugugu +"&dataTerm=month&pageNo=3&numOfRows=1&ServiceKey=DZ2bt4fg7QwJULCmQrMY0R1hRCJ8zNpPGzLWL7s1%2FpI04Ih5gF6ggxbvvBpBkCoYSFlAOccv2kVnMUc7BNMOIw%3D%3D&ver=1.3&_returnType=json",
+	    	url: "//openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=" + gugugu +"&dataTerm=month&pageNo=3&numOfRows=1&ServiceKey=DZ2bt4fg7QwJULCmQrMY0R1hRCJ8zNpPGzLWL7s1%2FpI04Ih5gF6ggxbvvBpBkCoYSFlAOccv2kVnMUc7BNMOIw%3D%3D&ver=1.3&_returnType=json",
 	        type: "get",
-	        dataType:"json",
+	        dataType:"jsonp",
+	        crossDomain: true,
 	        success:function(data){
 	           $(data).each(function(){
 	        	   var mise = this.list[0].pm25Grade; 		  //미세먼지등급
@@ -124,9 +127,10 @@ function successCallback(position) {
 	     })
 	     
 	      $.ajax({
-	        url:"http://203.247.66.146/iros/RetrieveLifeIndexService/getUltrvLifeList?ServiceKey=DZ2bt4fg7QwJULCmQrMY0R1hRCJ8zNpPGzLWL7s1%2FpI04Ih5gF6ggxbvvBpBkCoYSFlAOccv2kVnMUc7BNMOIw%3D%3D&base_date=20170613&areaNo=1100000000&_type=json",
+	        url:"//203.247.66.146/iros/RetrieveLifeIndexService/getUltrvLifeList?ServiceKey=DZ2bt4fg7QwJULCmQrMY0R1hRCJ8zNpPGzLWL7s1%2FpI04Ih5gF6ggxbvvBpBkCoYSFlAOccv2kVnMUc7BNMOIw%3D%3D&base_date=20170613&areaNo=1100000000&_type=json",
 	        type: "get",
-	        dataType:"json",
+	        dataType:"jsonp",
+	        crossDomain: true,
 	        success:function(data){
 	           $(data).each(function(){
 	        	   var uv = this.Response.body.indexModel.today; //자외선지수
