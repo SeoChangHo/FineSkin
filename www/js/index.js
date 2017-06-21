58dcf1ea862db63e3ebdc9d0409143a41fc0003b
SeoChangHo <sch1774@nate.com>
2017-06-21 16:51:12 +0900

    1651

--- www/js/index.js
+++ www/js/index.js
@@ -37,51 +37,11 @@ function successCallback(position) {
         var gujuso = "";
         
         console.log(lat + ", " + lng);
-        
-        $.ajax({
-	        url:"//api.openweathermap.org/data/2.5/weather?lat=" +lat + "&lon=" + lng +"&APPID=92e7b2df78fb8de10187e0abd8c5a46b",
-	        type: "get",
-	        crossDomain: true,
-	        dataType:"jsonp",
-	        success:function(data){
-	           $(data).each(function(){
-	              var weather = this.weather[0].main;    //날씨
-	              var sky = this.weather[0].description;   //하늘상태
-	              var temp = this.main.temp - 273.15;   //기온
-	              var pressure = this.main.pressure;       //압력
-	              var humidity = this.main.humidity;      //습도
-	              var clouds = this.clouds.all;				//구름
-	              
-	            	  
-	            	  if(weather=="Clear"){
-	            		  $(".wlist_txt1").html("날씨 : 맑음");  
-	            	  }else if(weather=="Clouds"){
-	            		  $(".wlist_txt1").html("날씨 : 구름");
-	            	  }else if(weather=="Haze"){
-	            		  $(".wlist_txt1").html("날씨 : 흐림");
-	            	  }else if(weather=="Rain"){
-	            		  $(".wlist_txt1").html("날씨 : 비");
-	            	  }else if(weather=="Fog"){
-	            		  $(".wlist_txt1").html("날씨 : 안개");
-	            	  }else{
-	            		  $(".wlist_txt1").html("날씨 : ?");
-	            	  }
-	            	  
-	            	  $(".wlist_txt2").html("온도 : " + temp.toFixed(2));
-	            	  $(".wlist_txt3").html("습도 : " + humidity);
-	           });
-	        },
-	        error: function(xhr, textStatus, errMsg){
-	           $("#weather").html("<p>" + textStatus + "(HTTP-" + xhr.status + " / " + errMsg + ") </p>");
-	        }
-	     })
-	     
+         
 	     $.ajax({
 	        url:"//apis.daum.net/local/geo/coord2addr?apikey=b9ab1680e352b1d4c5fe2b578928060e&longitude=" + lng + "&latitude=" + lat + "&inputCoordSystem=WGS84&output=json",
-	        async:false,
 	        type: "get",
 	        dataType:"jsonp",
-	        crossDomain: true,
 	        success:function(data){
 	           $(data).each(function(){
 	        	   sijuso = this.name1;   //시주소
@@ -90,6 +50,32 @@ function successCallback(position) {
 	              
             	  console.log(sijuso + " " + gujuso + " " + name3);
             	  $(".location").html(gujuso + " " + name3);
+            	  
+            	  var gugugu = encodeURIComponent(gujuso);
+            	  
+            	  $.ajax({
+          	        url:"https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='"+gugugu +", il')&format=json",
+          	        type: "get",
+          	        dataType:"jsonp",
+          	        success:function(data){
+          	           $(data).each(function(){
+          	              var weather = this.query.results.channel.item.condition.code;    //날씨코드
+          	              var weathertext = this.query.results.channel.item.condition.text; //날씨텍스트
+          	              var temp = this.query.results.channel.item.condition.temp;    //기온
+          	              var humidity = this.query.results.channel.atmosphere.humidity;     //습도
+          	              
+          	            	  
+          	            	  $(".wlist_txt1").html("날씨 : " + weather);
+          	            	  $(".wlist_txt2").html("온도 : " + ((temp-32)/1.8).toFixed(1));
+          	            	  $(".wlist_txt3").html("습도 : " + humidity);
+          	           });
+          	        },
+          	        error: function(xhr, textStatus, errMsg){
+          	           $("#weather").html("<p>" + textStatus + "(HTTP-" + xhr.status + " / " + errMsg + ") </p>");
+          	        }
+          	     })
+            	  
+            	  
 	           });
 	        },
 	        error: function(xhr, textStatus, errMsg){
@@ -97,28 +83,30 @@ function successCallback(position) {
 	        }
 	     })
 	     
-	     var gugugu = encodeURIComponent(gujuso);
-	     
 	     $.ajax({
-	    	url: "//openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=" + gugugu +"&dataTerm=month&pageNo=3&numOfRows=1&ServiceKey=DZ2bt4fg7QwJULCmQrMY0R1hRCJ8zNpPGzLWL7s1%2FpI04Ih5gF6ggxbvvBpBkCoYSFlAOccv2kVnMUc7BNMOIw%3D%3D&ver=1.3&_returnType=json",
+	    	url: "https://api.waqi.info/feed/geo:" + lat +";" + lng +"/?token=c5341d7416d6afdaa504f77aab84acc3c38bb8a8",
 	        type: "get",
 	        dataType:"jsonp",
-	        crossDomain: true,
 	        success:function(data){
 	           $(data).each(function(){
-	        	   var mise = this.list[0].pm25Grade; 		  //미세먼지등급
-	        	   var misenong = this.list[0].pm10Value; //미세먼지농도
+	        	   var mise = this.data.iaqi.pm25.v;
 	        	   
-	        	  if(mise==1){
+	        	  if(mise <= 50){
 	        		  $(".wlist_txt5").html("미세먼지: 좋음");  
-	        	  }else if(mise==2){
+	        	  }else if(mise <= 100){
 	        		  $(".wlist_txt5").html("미세먼지: 보통");
 	        		  $("<p>").html("미세먼지보통").appendTo(".messagetext");
-	        	  }else if(mise==3){
+	        	  }else if(mise<= 150){
+	        		  $(".wlist_txt5").html("미세먼지: 민감");
+	        		  $("<p>").html("미세먼지에 민감하신분은 조심!!").appendTo(".messagetext");
+	        	  }else if(mise<= 200){
 	        		  $(".wlist_txt5").html("미세먼지: 나쁨");
-	        	  }else if(mise==4){
+	        	  }else if(mise<=300){
 	        		  $(".wlist_txt5").html("미세먼지:매우나쁨");
+	        	  }else{
+	        		  $(".wlist_txt5").html("미세먼지:위험");
 	        	  }
+	        	  
 	           });
 	        },
 	        error: function(xhr, textStatus, errMsg){
@@ -129,8 +117,7 @@ function successCallback(position) {
 	      $.ajax({
 	        url:"//203.247.66.146/iros/RetrieveLifeIndexService/getUltrvLifeList?ServiceKey=DZ2bt4fg7QwJULCmQrMY0R1hRCJ8zNpPGzLWL7s1%2FpI04Ih5gF6ggxbvvBpBkCoYSFlAOccv2kVnMUc7BNMOIw%3D%3D&base_date=20170613&areaNo=1100000000&_type=json",
 	        type: "get",
-	        dataType:"jsonp",
-	        crossDomain: true,
+	        dataType:"json",
 	        success:function(data){
 	           $(data).each(function(){
 	        	   var uv = this.Response.body.indexModel.today; //자외선지수
